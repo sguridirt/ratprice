55a2e50e7ab36465dd20c4352f544308e4f6260d
sguridirt <34144826+sguridirt@users.noreply.github.com>
2021-02-25 08:36:03 -0300

    simplify project structure

--- ParisScrapper.py
+++
@@ -1,9 +0,0 @@
-from requests_html import HTMLSession
-
-session = HTMLSession()
-
-
-def get_price(url):
-    r = session.get(url)
-    price_selector = "#pdpMain > div.row.row-flex.pdp-main-info > div.col-xs-12.col-sm-12.col-md-6.col-lg-5.info-product-detail > div > div.col-xs-12.product-price-2 > div.col-md-6.col-xs-7.price.noPad > div.item-price"
-    return r.html.find(price_selector, first=True).text.split(" ")[0].replace("$", "")
--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import os
 from email import message
 import smtplib, ssl
-
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-from ParisScrapper import get_price
+from requests_html import HTMLSession
+
+session = HTMLSession()
+
+
+def get_price(url):
+    r = session.get(url)
+    price_selector = "#pdpMain > div.row.row-flex.pdp-main-info > div.col-xs-12.col-sm-12.col-md-6.col-lg-5.info-product-detail > div > div.col-xs-12.product-price-2 > div.col-md-6.col-xs-7.price.noPad > div.item-price"
+    return r.html.find(price_selector, first=True).text.split(" ")[0].replace("$", "")
+
 
 port = 465
 context = ssl.create_default_context()
@@ -27,10 +35,6 @@ html = """\
         <p><a href="{2}">{0}</a>'s current price is: $<b>{1}</b></p>
     </body>
 </html>"""
-
-
-def save_price():
-    pass
 
 
 def send_info(receiver, data):
