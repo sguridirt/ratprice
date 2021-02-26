d364441bc9fd270d307a06f477b462a011044ab6
sguridirt <34144826+sguridirt@users.noreply.github.com>
2021-02-26 09:27:00 -0300

    cleanup test code

--- price-engine/database.py
+++ price-engine/database.py
@@ -9,16 +9,3 @@ cred = credentials.Certificate(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-# # Create user
-# user_ref = db.collection("users").add(
-#     {"name": "Francisco", "email": "fguridirt@gmail.com"}
-# )
-
-# # Check product prices
-# product_ref = db.collection("products").document("jE3ZJPLjbSuu66PgPG7K")
-# price_docs = product_ref.collection("prices").stream()
-
-# for price in price_docs:
-#     print(price)
-#     print(f"{price.id} => {price.to_dict()}")
