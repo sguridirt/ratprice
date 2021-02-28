43f66c66e01c1d82cda326849eceec236657a2cc
sguridirt <34144826+sguridirt@users.noreply.github.com>
2021-02-28 13:00:03 -0300

    add helper function to create a product

--- price-engine/database.py
+++ price-engine/database.py
@@ -4,8 +4,16 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+from models import Product
+
 # Use a service account
 cred = credentials.Certificate(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+
+def create_product(name, url):
+    product = Product(None, name, url)
+    product_ref = db.collection("products").add(product.to_dict())
+    return product_ref
