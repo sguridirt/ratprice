f42624f8f67c1a1e30228e2893c75e6ff7082fa6
sguridirt <34144826+sguridirt@users.noreply.github.com>
2021-03-02 08:27:04 -0300

    share the HTMLSession and check if price exists
    This adds a little bit of resilience to the program

--- price-engine/scrappers/pcfactory.py
+++ price-engine/scrappers/pcfactory.py
@@ -1,13 +1,16 @@
-from requests_html import HTMLSession
-
-session = HTMLSession()
+from loguru import logger
 
 
 def parse_price(price):
     return int(price.replace("$", "").replace(".", "").strip())
 
 
-def get_price(url):
+def get_price(session, url):
     r = session.get(url)
     price_selector = "#id_ficha_producto > div.lp-cuerpo > div.ficha_cuerpo > div > div.ficha_producto_right > div.ficha_producto_precio_wrap > div.ficha_precio_normal > h2"
-    return parse_price(r.html.find(price_selector, first=True).text)
+    price = r.html.find(price_selector, first=True)
+    if price:
+        return parse_price(price.text)
+    else:
+        logger.warning("> (!) price not found.")
+        return None
