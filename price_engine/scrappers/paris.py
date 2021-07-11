c06970828233ffb840cc71a40ca9ff92d79aa52a
sguridirt <34144826+sguridirt@users.noreply.github.com>
2021-07-11 10:45:46 -0400

    update Paris price element selector

--- price_engine/scrappers/paris.py
+++ price_engine/scrappers/paris.py
@@ -7,7 +7,7 @@ def parse_price(price_str):
 
 def get_price(session, url):
     r = session.get(url)
-    price_selector = "#pdpMain > div.row.row-flex.pdp-main-info > div.col-xs-12.col-sm-12.col-md-6.col-lg-5.info-product-detail > div > div.col-xs-12.product-price-2 > div.col-md-6.col-xs-7.price.noPad > div.item-price"
+    price_selector = "#pdpMain > div.row.row-flex.pdp-main-info > div.col-xs-12.col-sm-12.col-md-6.col-lg-5.info-product-detail > div > div.col-xs-12.product-price-2 > div > div > div.price__text-wrap.price__text-wrap--primary > div.price__text"
     price = r.html.find(price_selector, first=True)
     if price:
         return parse_price(price.text)
