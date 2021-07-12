6869a036b2c6629886f7a5471073b4b2020d50cb
sguridirt <34144826+sguridirt@users.noreply.github.com>
2021-07-12 10:22:54 -0400

    add URL filter to product registration

---
+++ price_engine/URLFilter.py
@@ -0,0 +1,17 @@
+import re
+from telegram.ext import MessageFilter
+
+
+class URLFilter(MessageFilter):
+    def filter(self, message):
+        regex = re.compile(
+            r"^(?:http|ftp)s?://"  # http:// or https://
+            r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+            r"localhost|"  # localhost...
+            r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+            r"(?::\d+)?"  # optional port
+            r"(?:/?|[/?]\S+)$",
+            re.IGNORECASE,
+        )
+
+        return re.match(regex, message.text) is not None
